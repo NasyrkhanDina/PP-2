54bffc249c872acb0c55028f953d4183abd7c299
Dina <dbom12360@gmail.com>
2018-01-18 13:54:03 +0600

    testing

---
+++ week2/ConsoleApp1/isprime/isprime.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static bool isPrime(int n)
+        {
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+
+                if (isPrime(int.Parse(args[i])))
+                    Console.WriteLine(args[i]);
+
+            Console.ReadKey();
+
+        }
+
+    }
+}

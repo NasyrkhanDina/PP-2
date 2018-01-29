92d4e684b42aee559d7b6d4771d716fa628b5e17
Dina <dbom12360@gmail.com>
2018-01-29 21:48:18 +0600

    class rectangle

---
+++ week1/ClassRectangle/classforRectangle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classforRectangle
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int w = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            Rectangle r = new Rectangle(w, h);
+            Console.WriteLine(r);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ week1/ClassRectangle/classforRectangle/rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classforRectangle
+{
+    class Rectangle
+    {
+        public int width, height, area, per;
+
+        public Rectangle(int w, int h)
+        {
+            this.width = w;
+            this.height = h;
+            findArea();
+            findPer();
+        }
+
+        public void findArea()
+        {
+            area = width * height;
+        }
+        public void findPer()
+        {
+            per = 2 * (width + height);
+        }
+
+        public override string ToString()
+        {
+            return area + " " + per;
+        }
+
+    }
+}

aa2c2810d0935f4219297285b201f9bc691c4447
Dina <dbom12360@gmail.com>
2018-02-06 03:41:47 +0600

    advanced complex

---
+++ week2/Complex/AdvancedComplex/AdvancedComplex/Complex.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedComplex
+{
+    class Complex
+    {
+        public int a, b;
+
+        public Complex() { }
+
+        public Complex(int _a, int _b)
+        {
+            a = _a;
+            b = _b;
+        }
+
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+        public Complex Add(Complex c)
+        {
+            Complex res = new Complex(this.a * c.b + this.b * c.a, this.b * c.b);
+            res.Simplify();
+            return res;
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.a * c2.b + c2.a * c1.b, c1.b * c2.b);
+            res.Simplify();
+            return res;
+        }
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.a * c2.b - c2.a * c1.b, c1.b * c2.b);
+            res.Simplify();
+            return res;
+        }
+        public static Complex operator *(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.a * c2.a, c1.b * c2.b);
+            res.Simplify();
+            return res;
+        }
+        public static Complex operator /(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.a * c2.b, c1.b * c2.a);
+            res.Simplify();
+            return res;
+        }
+
+        public void Simplify()
+        {
+            int _a = this.a;
+            int _b = this.b;
+
+            while (_a > 0 && _b > 0)
+            {
+                if (_a > _b)
+                {
+                    _a = _a % _b;
+                }
+                else
+                {
+                    _b = _b % _a;
+                }
+            }
+            int gcd = _a + _b;
+            this.a /= gcd;
+            this.b /= gcd;
+        }
+    }
+}
---
+++ week2/Complex/AdvancedComplex/AdvancedComplex/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedComplex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] ss = s.Split(' ');
+            string a = ss[0];
+            string b = ss[1];
+            string[] c = a.Split('/');
+            string[] d = b.Split('/');
+
+            int a1 = int.Parse(c[0]);
+            int a2 = int.Parse(c[1]);
+            int b1 = int.Parse(d[0]);
+            int b2 = int.Parse(d[1]);
+
+            Complex kek1 = new Complex(a1, a2);
+            Complex kek2 = new Complex(b1, b2);
+
+            Complex kek3 = kek1.Add(kek2);
+            Complex kek4 = kek1 + kek2;
+            Complex kek5 = kek1 - kek2;
+            Complex kek6 = kek1 * kek2;
+            Complex kek7 = kek1 / kek2;
+
+            Console.WriteLine("add" + " " + kek3);
+            Console.WriteLine("plus" + " " + kek4);
+            Console.WriteLine("minus" + " " + kek5);
+            Console.WriteLine("multiply" + " " + kek6);
+            Console.WriteLine("division" + " " + kek7);
+
+            Console.ReadKey();
+        }
+    }
+}

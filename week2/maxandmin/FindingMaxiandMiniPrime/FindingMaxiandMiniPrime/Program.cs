db381b0fa9f70e6192ee95e0d0a9959a30bd52e8
Dina <dbom12360@gmail.com>
2018-02-06 02:15:57 +0600

    new files for 2nd lab

---
+++ week2/maxandmin/FindingMaxiandMiniPrime/FindingMaxiandMiniPrime/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Compag\Desktop\kek.txt");
+            string s = sr.ReadToEnd();
+            string[] a = s.Split(' ');
+
+            int mini = 10000;
+            int maxi = -100;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                int test = int.Parse(a[i]);
+                bool k = true;
+
+                if (test == 1)
+                {
+                    continue;
+                }
+
+                else
+                {
+                    for (int j = 2; j <= Math.Sqrt(test); j++)
+                    {
+                        if (test % j == 0)
+                        {
+                            k = false;
+                        }
+                    }
+                }
+                if (k)
+                {
+                    mini = Math.Min(test, mini);
+                    maxi = Math.Max(test, maxi);
+                }
+            }
+            StreamWriter sw = new StreamWriter(@"C:\Users\Compag\Desktop\answer.txt");
+            sw.WriteLine(mini);
+            sw.WriteLine(maxi);
+            sr.Close();
+            sw.Close();
+            Console.ReadKey();
+        }
+    }
+}

b6a5ef0b70cc975dd1ba2f6c9bc047a91aa8d36d
Dina <dbom12360@gmail.com>
2018-02-07 13:13:27 +0600

    new files for deserialization

---
+++ week4/XMLSerializationForComplex/XMLSerializationForComplex/Complex.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLSerializationForComplex
+{
+    class Complex
+    {
+        public int a, b;
+        public Complex() { }
+        public Complex(int _a, int _b)
+        {
+            this.a = _a;
+            this.b = _b;
+        }
+        public Complex Add(Complex c)
+        {
+            Complex res = new Complex(this.a + c.a, this.b + c.a);
+            res.Simplify();
+            return res;
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.b * c2.a + c2.b * c1.a, c1.b * c2.b);
+            res.Simplify();
+            return res;
+        }
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.b * c2.a - c2.b * c1.a, c1.b * c2.b);
+            res.Simplify();
+            return res;
+        }
+        public static Complex operator *(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.a * c2.a, c1.b * c2.b);
+            res.Simplify();
+            return res;
+        }
+        public static Complex operator /(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(c1.a * c2.b, c1.b * c2.a);
+            res.Simplify();
+            return res;
+        }
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+        public void Simplify()
+        {
+            int _a = this.a;
+            int _b = this.b;
+            while (_a > 0 && _b > 0)
+            {
+                if (_a > _b)
+                {
+                    _a %= _b;
+                }
+                else
+                {
+                    _b %= _a;
+                }
+            }
+            int gcd = _a + _b;
+            this.a /= gcd;
+            this.b /= gcd;
+        }
+    }
+}
---
+++ week4/XMLSerializationForComplex/XMLSerializationForComplex/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLSerializationForComplex
+{
+   
+    class Program
+    {
+        static void Xmls()
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(Complex));
+            FileStream fs = new FileStream("data.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            string line = Console.ReadLine();
+            string[] s = line.Split(' ');
+            string a = s[0];
+            string b = s[1];
+            string[] d = a.Split('/');
+            string[] v = b.Split('/');
+
+            int a1 = int.Parse(d[0]);
+            int a2 = int.Parse(d[1]);
+            int b1 = int.Parse(v[0]);
+            int b2 = int.Parse(v[1]);
+
+            Complex t1 = new Complex(a1, a2);
+            Complex t2 = new Complex(b1, b2);
+            Complex t3 = t1.Add(t2);
+            Complex t4 = t1 - t2;
+            Complex t5 = t1 * t2;
+            Complex t6 = t1 / t2;
+            t3.Simplify();
+            t4.Simplify();
+            t5.Simplify();
+            t6.Simplify();
+            
+            try
+            {
+                xs.Serialize(fs, t3);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                fs.Close();
+            }
+            Console.ReadKey();
+        }
+        
+        static void Deserialization()
+        {
+            string line = Console.ReadLine();
+            string[] s = line.Split(' ');
+            string a = s[0];
+            string b = s[1];
+            string[] d = a.Split('/');
+            string[] v = b.Split('/');
+
+            int a1 = int.Parse(d[0]);
+            int a2 = int.Parse(d[1]);
+            int b1 = int.Parse(v[0]);
+            int b2 = int.Parse(v[1]);
+
+            Complex t1 = new Complex(a1, a2);
+            Complex t2 = new Complex(b1, b2);
+            Complex t3 = t1.Add(t2);
+            Complex t4 = t1 - t2;
+            Complex t5 = t1 * t2;
+            Complex t6 = t1 / t2;
+            t3.Simplify();
+            t4.Simplify();
+            t5.Simplify();
+            t6.Simplify();
+            
+            XmlSerializer xs = new XmlSerializer(typeof(Complex));
+            FileStream fs = new FileStream("data.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            Complex c = xs.Deserialize(fs) as Complex;
+            fs.Close();
+            Console.WriteLine("Multplication" + "  " + t5);
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Xmls();
+            Deserialization();
+
+        }
+    }
+}

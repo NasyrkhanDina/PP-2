bfa4e573bc07db8a58a7c406d8f55563b63b21e8
Dina <dbom12360@gmail.com>
2018-03-04 14:11:17 +0600

    new changes

--- Bonus_Task/Bonus_Task/Program.cs
+++ Bonus_Task/Bonus_Task/Program.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Xml.Serialization;
 
 namespace Bonus_Task
 {
     class Program
     {
+        static void F1(Shop shop)
+        {
+            FileStream fs = new FileStream(@"data.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xml = new XmlSerializer(typeof(Shop));
+            xml.Serialize(fs, shop);
+            fs.Close();
+        } 
+        static Shop F2()
+        {
+            FileStream fs = new FileStream(@"data.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xml = new XmlSerializer(typeof(Shop));
+            Shop sh = xml.Deserialize(fs) as Shop;
+            fs.Close();
+            return sh;
+
+        }
       
 
         static void Main(string[] args)
@@ -19,14 +36,21 @@ namespace Bonus_Task
             Product product = new Product();
             Shop shop = new Shop();
             List<Product> list = new List<Product>();
+            
 
             while (true)
             {
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine("Welcome to our shop");
+
+                
                 Console.SetCursorPosition(20, 12);
                 Console.ForegroundColor = ConsoleColor.Green;
+
                 Console.WriteLine("Press [Probel] to continue ");
+
+                Console.WriteLine("If you want to continue your last shop, please press [1]");
+
                 ConsoleKeyInfo info = Console.ReadKey();
                 if (info.Key == ConsoleKey.Spacebar)
                 {
@@ -46,6 +70,8 @@ namespace Bonus_Task
 
                     Console.SetCursorPosition(0, 25);
                     Console.WriteLine("If you want to buy a book press [C] after write the name of the book");
+                    Console.SetCursorPosition(0, 27);
+                    Console.WriteLine("If you want to save changes press [2]");
                     ConsoleKeyInfo keyInfo = Console.ReadKey();
 
                     if(keyInfo.Key == ConsoleKey.C)
@@ -56,19 +82,23 @@ namespace Bonus_Task
                         shop.TheAmountOfBooks();
 
                     }
+                    if(keyInfo.Key == ConsoleKey.NumPad2)
+                    {
+                        F1(shop);
+                    }
+                  
 
 
                 }
+                if (info.Key == ConsoleKey.NumPad1)
+                {
+                    shop = F2();
+                }
+
+
                 Console.ReadKey();
 
             }
-
-
-            
-
-
-
-           // DirectoryInfo info = new DirectoryInfo(@"C:\Users\Compag\Desktop\PP-2\Bonus_Task\Bonus_Task\Shop.txt");
         }
     }
 }

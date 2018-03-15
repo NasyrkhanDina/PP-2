44cb61451007b6bcb7c130459c307a4b371d7497
Dina <dbom12360@gmail.com>
2018-03-16 02:48:27 +0600

    asd

---
+++ week(9-10)/Operationsfor2numbers/Operationsfor2numbers/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Operationsfor2numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result.Text = (sum((int.Parse(textBox1.Text)), (int.Parse(textBox2.Text)))).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            result.Text = (minus((int.Parse(textBox1.Text)), (int.Parse(textBox2.Text)))).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            result.Text = (multiply((int.Parse(textBox1.Text)), (int.Parse(textBox2.Text)))).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            result.Text = (divide((int.Parse(textBox1.Text)), (int.Parse(textBox2.Text)))).ToString();
+        }
+
+        public static int sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static int minus(int a, int b)
+        {
+            return a - b;
+        }
+        public static int multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static int divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
